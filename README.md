4bfd1fe630102ec9081fabca990e8fbcd7803654
DURDONA <holisihlos@gmail.com>
2018-09-07 21:47:46 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # FEND-Matching-Game-Project
+## What Will I Build?
+
+<p>The Memory Game Project is all about demonstrating your mastery of JavaScript. You’ll build a complete browser-based card matching game (also known as Concentration). But this isn’t just any memory game! It’s a shnazzy, well-designed, feature-packed memory game!</p>
+
+## How the Game Works
+ <p>The game board consists of sixteen "cards" arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol face down. The gameplay rules are very simple: flip over two hidden cards at a time to locate the ones that match!</p>
+
+ <p>Each turn:</p>
+
+*  The player flips one card over to reveal its underlying symbol.
+*  The player then turns over a second card, trying to find the corresponding card with the same symbol.
+*  If the cards match, both cards stay flipped over.
+*  If the cards do not match, both cards are flipped face down.
+
+<p>The game ends once all cards have been correctly matched.</p>
+
+## Game Functionality
+ <p>The real-life game, players flip over cards to locate the pairs that match The goal is to recreate this effect in your project. There are a couple of interactions that you'll need to handle:</p>
+
+* Flipping cards
+* What happens when cards match
+* What happens when cards do not match
+* When the game finishes
+
+## Interactivity Guideline
+
+<p>Keep in mind that the specific functionality demonstrated here (e.g. a card performs a horizontal flip when clicked) is an example. You do not need to have this exact functionality.</p>
+
+## What Will I Learn?
+
+<p>The memory game presents the first opportunity to fully combine your skills in HTML, CSS, and JavaScript into a large project. Aside from solidifying your skills with these three technologies, you'll discover how best to combine them in a complex application.</p>
+<p>The following are just some of the questions that you'll experience along the way:</p>
+
+* What's the ideal workflow?
+* How many files do I need?
+* Do I modify the HTML first or the CSS?
+* How many JavaScript functions do I need?
+* Should my function be this many lines of code?
+
+<p>There's no one right answer to each question. While building this project, working with mentors, and getting feedback from the project reviewer you'll develop answers to these questions.</p>
---
+++ js/scripts.js
@@ -0,0 +1,116 @@
+﻿let card_values = ['australia', 'bolivia', 'california', 'hawaii', 'italy', 'norway', 'paris', 'china']; // cards saved in array so that we can easily have access to them 
+
+let state; // Game state (see restart).
+let disabled = false; // Clicking disabled (during animation).
+
+let rating, timer, moves; // Interface elements (not to be confused with state).    // line from 2-7 has global variable which can be accessed from anywhere in the code 
+
+
+
+function shuffle(cards) { // Fisher-Yates shuffle.      // function has a parameter (cards) which can be accessed withing function shuffle, called local scope 
+        for (let i = cards.length - 1; i > 0; --i) {  
+                let j = Math.floor((i + 1) * Math.random()); // Math.floor along with Math.random are JavaScript defined functions which randomly shuffles cards and returns cards value which will be store in variable 'j'
+                let temp = cards[i];
+                cards[i] = cards[j];  
+                cards[j] = temp; 
+        }
+        return cards; 
+}
+
+
+
+function update_rating() { // regular function  with no parameters 
+        rating.textContent = state.moves < 16 ? '★★★' : state.moves < 24 ? '★★' : '★'; // .textContent property is used to get the content of the 'state' variable and combination with global variable moves will print
+} // stars based on number of moves user makes. You used Conditional (ternary) Operator to test it condition  
+
+function update_timer() {
+        timer.textContent = Math.round((performance.now() - state.start) / 1000); // where did we get 'performance' from ... (1000) - indicates delay in ms I think 
+}
+
+function update_moves() {
+        moves.textContent = state.moves;
+        update_rating();
+}
+
+
+
+function restart() { // Reset game state and shuffle cards.
+        state = {
+                selected: null, // Currently selected card.
+                matches: 0, // Number of matches found.
+                start: performance.now(), // Game start time.
+                moves: 0, // Number of moves.
+        };
+        const cards = shuffle(Array.from(document.getElementsByClassName('card')));
+        for (let i = 0; i < card_values.length; ++i) {
+                for (let j = 0; j < 2; ++j) {
+                        cards[2 * i + j].setAttribute('card', card_values[i]);
+                        cards[2 * i + j].removeAttribute('show');
+                        cards[2 * i + j].removeAttribute('done');
+                }
+        }
+        update_moves();
+        update_timer();
+}
+
+function congratulate() { // Congratulate player and ask to play again.
+        if (confirm('Rating: ' + rating.textContent + ' Time: ' + timer.textContent + ' Play again?'))
+                restart();
+}
+
+function make_card() { // Create new card element from template and set onclick.
+        const node = document.importNode(document.getElementById('card').content, true);
+        const card = node.getElementById('div');
+
+        card.onclick = function() {
+                if (disabled || card.getAttribute('done')) // Ignore invalid clicks.
+                        return;
+                else if (!state.selected) { // Select first card.
+                        card.setAttribute('show', true);
+                        state.selected = card;
+                } else if (card != state.selected) { // Select second card.
+                        ++state.moves;
+                        update_moves();
+                        disabled = true; // Disable further clicks until timeout.
+                        var temp = state.selected; // Save a reference to selected card,
+                        state.selected = null; // and reset actual selection.
+                        card.setAttribute('show', true);
+                        if (temp.getAttribute('card') == card.getAttribute('card')) { // Match.
+                                let done = ++state.matches == card_values.length;
+                                setTimeout(function() {
+                                        temp.setAttribute('done', true);
+                                        card.setAttribute('done', true);
+                                        disabled = false;
+                                        if (done)
+                                                congratulate();
+                                }, 1000);
+                        } else { // No match.
+                                setTimeout(function() {
+                                        temp.removeAttribute('show');
+                                        card.removeAttribute('show');
+                                        disabled = false;
+                                }, 1000);
+                        }
+                }
+        };
+        return card;
+}
+
+
+
+onload = function() { // Do this when document loads.
+        // Get all necessary elements.
+        rating = document.getElementById('rating');
+        timer = document.getElementById('timer');
+        moves = document.getElementById('moves');
+        let button_restart = document.getElementById('restart');
+        // Set callbacks.
+        button_restart.onclick = restart;
+        setInterval(update_timer, 1000);
+        // Make cards.
+        const cards = document.getElementById('cards');
+        for (let i = 0; i < 2 * card_values.length; ++i)
+                cards.appendChild(make_card());
+        // Start game.
+        restart();
+}
